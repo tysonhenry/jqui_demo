9820feb4370b6f0aca30fca006c4c3986e376f5f
tysonhenry <tysonhenry@gmail.com>
2015-11-06 01:19:38 +0000

    adds comment to home.js

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,8 +1,10 @@
 // the following code was copied from the jqueryui.com examples
 
+// $(function() is shorthand for $( document ).ready()
 $(function() {
   $( "#accordion" ).accordion();
 });
+
 $(function() {
   $( "#accordion2" ).accordion();
 });
